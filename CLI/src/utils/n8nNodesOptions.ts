6a74b23799d5a36f6456c853e6ecfcd090548a01
Batikan-Kutluer <60034174+Batikan-Kutluer@users.noreply.github.com>
2025-07-05 11:00:53 +0300

    CLI Improved

---
+++ CLI/src/utils/n8nNodesOptions.ts
@@ -0,0 +1,7 @@
+export const n8nNodesOptions = [
+  { value: "The Graph", label: "The Graph" },
+  { value: "Walrus", label: "Walrus" },
+  { value: "Polymarket", label: "Polymarket" },
+  { value: "1inch", label: "1inch" },
+  { value: "Flare Network", label: "Flare Network" },
+];
