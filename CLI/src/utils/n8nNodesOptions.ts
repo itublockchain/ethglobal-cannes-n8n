1fc2eef1608ca4aa6dc6e5f9fa2806b44e7149f3
UYUYANMARUL <ataberkcekic@hotmail.com.tr>
2025-07-06 06:28:24 +0200

    fix(euler cli)

--- CLI/src/utils/n8nNodesOptions.ts
+++ CLI/src/utils/n8nNodesOptions.ts
@@ -5,4 +5,5 @@ export const n8nNodesOptions = [
   { value: "RelayNode", label: "Relay" },
   { value: "Hardhat", label: "Hardhat" },
   { value: "AaveNode", label: "Aave" },
+  { value: "EulerNode", label: "Euler" },
 ];
